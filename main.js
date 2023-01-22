d90613bf6d1c38a3c4ee240ca3177f5c102fcc01
Wilton <wiltoncamilol@gmail.com>
2023-01-22 15:33:27 -0500

    finished

--- main.js
+++ main.js
@@ -5,5 +5,19 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
-
 // You may write your code here!
+const allColor = document.querySelectorAll("#palette div.color");
+allColor.forEach((colorElement) =>  {
+ colorElement.addEventListener('click', () => {
+  const currentColor = document.querySelector('#current-color');
+  currentColor.style.background = colorElement.style.background;
+ });
+});
+// create variable to grab node list cells (cells not in css)
+const allCells = document.querySelectorAll('#canvas div.cell');
+allCells.forEach((cell) => {
+  cell.addEventListener('mouseover', (event) => {
+    const currentColor = document.querySelector('#current-color');
+event.target.style.background = currentColor.style.background
+  });
+});
